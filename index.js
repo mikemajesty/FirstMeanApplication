31939cdf4a762868f16b015aefa584b526b6c393
mikemajesty <mikemajesty2011@gmail.com>
2016-04-23 15:53:39 -0300

    Add libs to the project

---
+++ index.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
