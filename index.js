4875b84fe083e16262d1272c4d354adb9a2894d5
mikemajesty <mikemajesty2011@gmail.com>
2016-04-23 16:18:13 -0300

    Remove unnecessary files

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+var f = ()=>{
+  
+};
